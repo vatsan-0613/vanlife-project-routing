54e5be4f282119fb5fddaa89366daf39f4effb21
SRIVATSAN S <95332677+vatsan-0613@users.noreply.github.com>
2023-08-27 16:40:15 +0530

    second commit

--- src/About.jsx
+++ src/About.jsx
@@ -1,12 +1,13 @@
 import Header from "./Header"
 import Footer from "./Footer"
+import aboutHero from "./assets/about-hero.jpg"
 
 
 export default function About(){
     return(
         <div>
             <Header />
-            <img className="about-hero" src="../public/images/about-hero.jpg" alt="" />
+            <img className="about-hero" src={aboutHero} alt="" />
             <div className="main-content">
                 <h2 className="about-title">Don’t squeeze in a sedan when you could relax in a van.</h2>
                 <p className="about-para">Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch.
